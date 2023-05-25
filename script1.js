dccdcf728852ce2199ce0add3e72e986095662fb
marcopires102 <marcopires2002@gmail.com>
2023-05-25 16:42:37 +0100

    versao 6

--- script1.js
+++ script1.js
@@ -49,3 +49,61 @@ http.onload = function(){
 		document.querySelector(".museus").innerHTML = output;
 	}
 } 
+
+const checkboxes = document.querySelectorAll('.categoria');
+checkboxes.forEach(checkbox => checkbox.addEventListener('change', filtrarMuseus));
+
+function filtrarMuseus() {
+  const categoriasSelecionadas = Array.from(checkboxes)
+    .filter(checkbox => checkbox.checked)
+    .map(checkbox => checkbox.value);
+  
+  const museusFiltrados = museus.filter(museu => {
+    if (categoriasSelecionadas.length === 0) {
+      return true; // Mostrar todos se nenhuma categoria estiver selecionada
+    } else {
+      return categoriasSelecionadas.includes(museu.categoria);
+    }
+  });
+
+  exibirMuseus(museusFiltrados);
+}
+
+function exibirMuseus(museus) {
+  let output = "";
+
+  for (let item of museus) {
+    output += `
+      <div class="row pt-5">
+        <div class="col-12 col-md-6 text-center" style="padding-right: 10px;">
+          <img src="${item.image}" alt="" class="img-fluid" style="max-width: 369px; height: auto;">
+        </div>
+        <div class="col-12 col-md-6 pt-3 text-left">
+          <div class="row">
+            <div class="col-12">
+              <p style="font-size: 30px;"><strong>${item.title}</strong></p>
+            </div>
+          </div>
+          <div class="row text-center">
+            <p style="font-size: 15px;">${item.distancia} &emsp;&emsp;&emsp;&emsp;&emsp;<i class="bi bi-star-fill"></i>
+              <i class="bi bi-star-fill"></i>
+              <i class="bi bi-star-fill"></i>
+              <i class="bi bi-star-fill"></i>
+              <i class="bi bi-star"></i>
+            </p>
+          </div>
+          <div class="row text-left">
+            <p style="font-size: 16px;">${item.descricao}</p>
+          </div>
+          <div class="row text-left">
+            <p style="font-size: 15px;"> <i class="bi bi-telephone-fill"></i>&emsp;<strong>${item.telemovel}</strong></p>
+          </div>
+        </div>
+      </div>
+    `;
+  }
+
+  document.querySelector(".museus").innerHTML = output;
+}
+
+
