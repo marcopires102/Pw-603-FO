a3442a3a997c11403ec36bfc14597280b2ea3e91
marcopires102 <marcopires2002@gmail.com>
2023-05-25 15:53:49 +0100

    Versao 5

--- script1.js
+++ script1.js
@@ -14,36 +14,34 @@ http.onload = function(){
 
 		for(let item of museus){
 			output += `
-          <div class="row pt-5">
-            <div class="col-2 col-md-7 text-center style="padding-right="10px">
-              <img src="${item.image}" alt="" class="img-fluid" style="width: 369px; height-auto: 249px;">
-            </div>
-            <div class="col-10 col-md-5 pt-5 text-left ">
-              <div class="row">
-                <div class="col-12">
-                  <p style="font-size: 40x;"><strong>${item.title}</strong></p>
-                </div>
-              </div>
-              <div class="row text-center ">
-                
-                  <p style="font-size: 11px;">${item.distancia} &emsp;&emsp;&emsp;&emsp;&emsp;<i class="bi bi-star-fill"></i>
-                    <i class="bi bi-star-fill"></i>
-                    <i class="bi bi-star-fill"></i>
-                    <i class="bi bi-star-fill"></i>
-                    <i class="bi bi-star"></i></p>
-                  
-                
-              </div>
-              <div class="row text-left">
-                <p style="font-size: 10px;">${item.descricao}</p>
-              </div>
-              <div class="row text-center ">
-                <p style="font-size: 11px;"> <i class="bi bi-telephone-fill"></i>&emsp;<strong>${item.telemovel}</strong></p>
-            
+      <div class="row pt-5">
+  <div class="col-12 col-md-6 text-center" style="padding-right: 10px;">
+    <img src="${item.image}" alt="" class="img-fluid" style="max-width: 369px; height: auto;">
+  </div>
+  <div class="col-12 col-md-6 pt-3 text-left" style="left: -50px">
+    <div class="row">
+      <div class="col-12">
+        <p style="font-size: 30px;"><strong>${item.title}</strong></p>
+      </div>
+    </div>
+    <div class="row text-center">
+      <p style="font-size: 15px;">${item.distancia} &emsp;&emsp;&emsp;&emsp;&emsp;<i class="bi bi-star-fill"></i>
+        <i class="bi bi-star-fill"></i>
+        <i class="bi bi-star-fill"></i>
+        <i class="bi bi-star-fill"></i>
+        <i class="bi bi-star"></i>
+      </p>
+    </div>
+    <div class="row text-left">
+      <p style="font-size: 16px;">${item.descricao}</p>
+    </div>
+    <div class="row text-left">
+      <p style="font-size: 15px;"> <i class="bi bi-telephone-fill"></i>&emsp;<strong>${item.telemovel}</strong></p>
+    </div>
+  </div>
+</div>
 
-              </div>
-            </div>
-          </div>
+    
 
 		`;
 		}
